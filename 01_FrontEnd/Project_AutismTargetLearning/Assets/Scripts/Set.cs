1f00ebce60f87350d94b18cf68f1eaa9ebf21e89
Spencer Kun <littlekundog@gmail.com>
2021-10-24 14:44:23 -0700

    Rotating set v1
    First version of rotating the set once. Unsure how it will work or if I got the correct file type, will work on in future.

--- 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/Set.cs
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/Set.cs
@@ -98,4 +98,12 @@ public class Set
 
         return (output);
     }
+    // Takes the last item in the set and stores it, then it removes it and then inserts it in the front making everything shift over 1.
+    public void rotateSet(List<Target> set)
+    {
+        int last;
+        last = set[set.Count - 1];
+        set.RemoveItem(set.Count - 1);
+        set.InsertItem(0, last);
+    }
 }
