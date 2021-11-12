d86a89f1f6cd4f5b4619710fbd5d81f32b3fb133
Kenneth Munk <contact.me@kenmunk.com>
2021-11-11 23:07:45 -0800

    Requirements For Message Script imported and script directory restructured

---
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/UtilityScripts/Imported/TempData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DataUtil
+{
+    public class Temp
+    {
+        //Fundamental data safe function to cut off data ties
+        public static object Obj(object data)
+        {
+            object temp = data;
+            return temp;
+        }
+
+        
+    }
+}
+
