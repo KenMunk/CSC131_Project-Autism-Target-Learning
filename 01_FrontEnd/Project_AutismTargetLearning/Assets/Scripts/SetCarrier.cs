cb893b691a922c9a640dfe9387e4fcd7a7381dab
Kenneth Munk <da.munkk@gmail.com>
2021-11-11 23:10:31 -0800

    Beginning work on moving set data between scenes

---
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/SetCarrier.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCarrier : MonoBehaviour
+{
+
+
+    void Awake() 
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getSet()
+    {
+
+    }
+
+    public void eraseCarrier()
+    {
+
+    }
+}
