e7ecb1bfa8bf25e5c8a7ac0f73ecc25924af9c83
Kenneth Munk <contact.me@kenmunk.com>
2021-11-11 22:34:23 -0800

    Importing message script from prior projects
    This was a solution that I've implemented in past projects in my asset building projects

---
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/Message.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataUtil;
+using System;
+
+/*
+
+Code Authored by Kenneth Munk
+2021-01-13
+
+Purpose:
+Transmitting data between gameobjects easier
+
+
+*/
+
+public class Message
+{
+    public GameObject sender;
+    public object data;
+    public Type dataType;
+
+    public Message(GameObject _sender, object _data)
+    {
+        this.sender = (GameObject)Temp.Obj(_sender);
+        this.data = Temp.Obj(_data);
+        this.dataType = _data.GetType();
+    }
+
+    public GameObject getSender()
+    {
+        return ((GameObject)Temp.Obj(this.sender));
+    }
+
+    public object getData()
+    {
+        return (Temp.Obj(this.data));
+    }
+
+    public Type getType()
+    {
+        return ((Type)Temp.Obj(this.dataType));
+    }
+
+    public void sendReply(string command, object _data)
+    {
+        this.getSender().SendMessage(command, _data);
+    }
+
+    
+}
