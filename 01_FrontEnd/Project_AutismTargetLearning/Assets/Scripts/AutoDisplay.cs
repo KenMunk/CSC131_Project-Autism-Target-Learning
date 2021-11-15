b7396b5e8304913c3c94ed248ff020b03f07e40d
Kenneth Munk <contact.me@kenmunk.com>
2021-11-15 01:51:50 -0800

    Planning the automatic layout system

--- 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/AutoDisplay.cs
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/AutoDisplay.cs
@@ -8,15 +8,24 @@ public class AutoDisplay : MonoBehaviour
     private float aspectRatio = 0;
 
     public Set targetSet = null;
+    private bool requestSent = false;
 
     public GameObject setError;
+
+    public GameObject TargetViewerPrefab;
+
+    public float desiredPadding = 50;
+    public float tileSideLength = 500;
+    public Rect targetViewingSpace;
+    public int rows = 1;
+    public int columns = 1;
     
     // Start is called before the first frame update
     void Start()
     {
         //Need to obtain the aspect ratio of the target display area
-        Rect targetSpace = gameObject.GetComponent<RectTransform>().rect;
-        aspectRatio = (Mathf.Ceil((targetSpace.width / targetSpace.height)*10))/10;
+        this.targetViewingSpace = gameObject.GetComponent<RectTransform>().rect;
+        aspectRatio = (Mathf.Ceil((this.targetViewingSpace.width / this.targetViewingSpace.height)*10))/10;
 
 
 
@@ -26,7 +35,7 @@ public class AutoDisplay : MonoBehaviour
     void Update()
     {
         //check if a target set has already been selected
-        if(targetSet == null)
+        if(targetSet == null && !requestSent)
         {
             try
             {
@@ -36,6 +45,7 @@ public class AutoDisplay : MonoBehaviour
                 {
                     //if the set transporter exists then get the set
                     setTransporter.SendMessage("getSet", new Message(gameObject, null));
+                    requestSent = true;
                 }
                 else
                 {
@@ -44,6 +54,7 @@ public class AutoDisplay : MonoBehaviour
             }
             catch (Exception e)
             {
+                Debug.LogFormat($"Exception detected: {e}");
                 setError.SetActive(true);
             }
         }
@@ -52,6 +63,22 @@ public class AutoDisplay : MonoBehaviour
     public void receiveSet(Set data)
     {
         this.targetSet = data;
+    }
+
+    public void determineSetLayout()
+    {
+        if(this.targetSet != null)
+        {
+            //first we'll prime the layout with the first possible layout and then continue reducing the size
+            //proportionate to the size used until the overall aspect ratio is less than the space aspect ratio
+            //which would hopefully make it so that the images being presented are always large enough to be usable.
+            int rows = 1;
+            int columns = this.targetSet.GetList().Count;
+            float rowHeight = this.targetViewingSpace.height / rows;
+            this.tileSideLength = rowHeight - 50;
+
+            //increase the rows until the columns are under the aspect ratio desireds
+        }
     }
 
     public void rotateTargets()
