3a82e2e0a9295e62f9b0c9abeb996282d4f54727
Kenneth Munk <contact.me@kenmunk.com>
2021-11-20 01:55:45 -0800

    set preview prefab code completed needs testing

--- 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/TargetScripts/SetPreview.cs
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/TargetScripts/SetPreview.cs
@@ -9,6 +9,8 @@ public class SetPreview : MonoBehaviour
     public GameObject TargetPreviewPrefab;
 
     public List<GameObject> targetsPreviewed = new List<GameObject>();
+    public float rotationLength = 600;
+    private int stage = 0;
 
     private int deploymentCursor = 0;
     // Start is called before the first frame update
@@ -20,12 +22,15 @@ public class SetPreview : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.deployImage();
+        this.sendSprites();
+        this.moveTargets();
     }
 
     public void setPreviewSet(Set setToPreview)
     {
         this.previewSet = setToPreview;
+        this.rotationLength = 325 * this.previewSet.GetList().Count;
     }
 
     public bool readyToPreview()
@@ -36,17 +41,65 @@ public class SetPreview : MonoBehaviour
 
     public void deployImage()
     {
-        if (this.deploymentCursor < this.previewSet.GetList().Count)
+        if (this.deploymentCursor < this.previewSet.GetList().Count && this.stage == 0)
         {
             GameObject previewImage = Instantiate(this.TargetPreviewPrefab);
             previewImage.name = string.Format($"{this.previewSet.GetName()}_Image_{this.deploymentCursor}");
             previewImage.transform.localPosition = new Vector3(150 + (325 * deploymentCursor), -200, 0);
             this.deploymentCursor++;
+
+            if(this.deploymentCursor < this.previewSet.GetList().Count)
+            {
+                this.stage = 1;
+            }
+
+            this.targetsPreviewed.Add(previewImage);
         }
+        
     }
 
     public void sendSprites()
     {
+        if(this.deploymentCursor < this.previewSet.GetList().Count && this.stage == 1)
+        {
+            this.targetsPreviewed[this.deploymentCursor].SendMessage("setDisplayImage", this.previewSet.GetList()[this.deploymentCursor]);
+            this.deploymentCursor++;
 
+            if(this.deploymentCursor < this.previewSet.GetList().Count)
+            {
+                this.stage = 2;
+            }
+        }
+    }
+
+    public void startRotating()
+    {
+        setPreviewSetPrefabToRotate(gameObject.name);
+    }
+
+    public static string previewSetPrefabToRotate = "";
+
+    public static void setPreviewSetPrefabToRotate(string previewSetCodeName)
+    {
+        previewSetPrefabToRotate = previewSetCodeName;
+    }
+
+    public void moveTargets()
+    {
+        if(gameObject.name == previewSetPrefabToRotate)
+        {
+            for (int i = 0; i < this.targetsPreviewed.Count; i++)
+            {
+                Vector3 currentPosition = this.targetsPreviewed[i].transform.localPosition;
+                if (currentPosition.x + 10 < this.rotationLength)
+                {
+                    this.targetsPreviewed[i].transform.localPosition = new Vector3((currentPosition.x + 10), currentPosition.y, 0);
+                }
+                else if (currentPosition.x + 10 >= this.rotationLength)
+                {
+                    this.targetsPreviewed[i].transform.localPosition = new Vector3(-150, currentPosition.y, 0);
+                }
+            }
+        }
     }
 }
