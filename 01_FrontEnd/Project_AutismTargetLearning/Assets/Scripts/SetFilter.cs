d9674a964db0ea965b1096f11d582d396c6e43cd
Connor Craig <redblackwrap@yahoo.com>
2021-12-06 13:17:33 -0800

    added filter function to the set editor

---
+++ 01_FrontEnd/Project_AutismTargetLearning/Assets/Scripts/SetFilter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetFilter : MonoBehaviour
+{
+
+    public GameObject filterInputField;
+    public GameObject libraryView;
+    private InputField filterText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        filterText = filterInputField.GetComponentInChildren<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        filter();
+    }
+
+    void filter()
+    {
+        foreach (Transform target in libraryView.transform)
+        {
+            if (!target.GetComponent<Image>().sprite.name.ToLower().Contains(filterText.text.ToLower()))
+            {
+                target.gameObject.SetActive(false);
+            }
+            else if(filterText.text == ""){
+                target.gameObject.SetActive(true);
+            }
+            else
+            {
+                target.gameObject.SetActive(true);
+            }
+        }
+    }
+}
